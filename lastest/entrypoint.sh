bc77b6152337717fc14e6b4c3405eeb79b636f82
hieutv <hieutv26@viettel.com.vn>
2021-07-16 16:46:02 +0700

    Update config

--- lastest/entrypoint.sh
+++ lastest/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "starting app";
-python3 $ODOO_BIN -c $ODOO_CONFIG
+python3 $ODOO_BIN -c $ODOO_CONFIG -r $POSTGRES_USER -w $PASSWORD --db_host $DB_HOST --db_port $DB_PORT --http-port $HTTP_PORT
